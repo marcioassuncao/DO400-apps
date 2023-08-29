43077af69e4772e6a4006e1961229044f8c8aad8
marcioassuncao <foco.marcio.assuncao@gmail.com>
2023-08-29 07:35:39 -0400

    Add tests

--- calculator-microservices/adder/src/test/java/com/redhat/training/AdderResourceTest.java
+++ calculator-microservices/adder/src/test/java/com/redhat/training/AdderResourceTest.java
@@ -1,12 +1,20 @@
 package com.redhat.training;
 
 import com.redhat.training.service.SolverService;
+import static io.restassured.RestAssured.given;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.client.exception.ResteasyWebApplicationException;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.hamcrest.CoreMatchers.is;
+
+import javax.ws.rs.WebApplicationException;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
+import javax.ws.rs.core.Response;
 
 @QuarkusTest
 @TestHTTPEndpoint(AdderResource.class)
@@ -15,5 +23,42 @@ public class AdderResourceTest {
     @InjectMock
     @RestClient 
     SolverService solverService;
+
+    @Test
+    public void simpleSum() {
+        Mockito.when(solverService.solve("2")).thenReturn(Float.valueOf("2"));
+        Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+
+        given()
+        .when().get("3/2")
+        .then()
+            .statusCode(200)
+            .body(is("5.0"));
+    }
+    
+    @Test
+    public void negativeSum() {
+        Mockito.when(solverService.solve("-2")).thenReturn(Float.valueOf("-2"));
+        Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+
+        given()    
+        .when().get("3/-2")
+        .then()
+            .statusCode(200)
+            .body(is("1.0"));    
+    }
+
+    @Test
+    public void wrongValue() {
+        WebApplicationException cause = new WebApplicationException("Unknown error", Response.Status.BAD_REQUEST);
+        Mockito.when(solverService.solve("a")).thenThrow( new ResteasyWebApplicationException(cause) );
+        Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+
+        given()    
+        .when().get("3/a")
+        .then()
+            .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+    
+    }
 
 }
